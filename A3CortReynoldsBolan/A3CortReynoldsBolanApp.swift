0118a17a99fa0bbf466158e191c45d8b04614ab2
cortreynoldsbolan <reynoldsbolan.c@gmail.com>
2026-07-21 12:13:17 -0400

    Initial Commit

---
+++ A3CortReynoldsBolan/A3CortReynoldsBolanApp.swift
@@ -0,0 +1,17 @@
+//
+//  A3CortReynoldsBolanApp.swift
+//  A3CortReynoldsBolan
+//
+//  Created by Cort Reynolds-Bolan on 2026-07-21.
+//
+
+import SwiftUI
+
+@main
+struct A3CortReynoldsBolanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
